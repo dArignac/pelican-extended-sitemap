2030f9e5676303347cb9dd85b9614142659e52ad
Alexander Herrmann <darignac@googlemail.com>
2014-06-02 17:02:20 +0200

    new release 0.2.0

--- extended-sitemap/__init__.py
+++ extended-sitemap/__init__.py
@@ -16,7 +16,7 @@ else:
     from urlparse import urljoin
 
 
-__version__ = '0.1.4'
+__version__ = '0.2.0'
 
 
 class SitemapGenerator(object):
--- setup.py
+++ setup.py
@@ -52,6 +52,13 @@ setup(
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Text Processing :: Markup'
     ]
 )
