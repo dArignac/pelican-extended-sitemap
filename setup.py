445f4214186314d55a5d9b87ad09f77e767eb3b5
Alexander Herrmann <darignac@googlemail.com>
2014-06-04 20:42:00 +0200

    pelican does not support 2.6 [skip ci]

--- setup.py
+++ setup.py
@@ -56,7 +56,6 @@ setup(
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.0',
         'Programming Language :: Python :: 3.1',
