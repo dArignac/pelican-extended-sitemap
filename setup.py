8971ea5849e342fa2034b1d6ca017c4fc9430a99
Alexander Herrmann <darignac@googlemail.com>
2014-06-04 21:04:12 +0200

    Jinja2 does not run well on Python 3.2

--- setup.py
+++ setup.py
@@ -57,9 +57,6 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Text Processing :: Markup'
